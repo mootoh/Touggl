a583f291a9f94052d4341d1d84880dcb37b74f36
Motohiro Takayama <mootoh@gmail.com>
2012-02-07 09:58:41 -0800

    [Fix #24582187] show a different view for a tag touch event.

--- src/net/mootoh/toggltouch/TagTouchActivity.java
+++ src/net/mootoh/toggltouch/TagTouchActivity.java
@@ -1,6 +1,6 @@
 package net.mootoh.toggltouch;
 
-import org.json.JSONException;
+import java.util.ArrayList;
 
 import android.app.Activity;
 import android.content.Context;
@@ -8,13 +8,22 @@ import android.content.Intent;
 import android.nfc.NfcAdapter;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
 public class TagTouchActivity extends Activity {
+    private String tagId = null;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.tag_touch);
         handleIntent(getIntent());
     }
 
@@ -23,16 +32,21 @@ public class TagTouchActivity extends Activity {
         if (! NfcAdapter.ACTION_TECH_DISCOVERED.equals(action))
             return;
 
-        final String tagId = getTagId(intent);
+        tagId = getTagId(intent);
         if (tagId == null)
             return;
 
+        TextView messageLabel = (TextView)findViewById(R.id.tagTouchMessageLabel);
         if (Tag.isBrandNew(tagId, this)) {
-            startSettingActivityWithTagId(tagId);
-            return;
+            setTitle("New Tag"); 
+            messageLabel.setText("New Tag");
+        } else {
+            messageLabel.setText("Existing Tag");
         }
-/*
- * tmp
+
+        setupTaskList();
+        /*
+         * tmp
         TogglApi api = new TogglApi(this);
         final TimeEntry timeEntry = pStorage.currentTimeEntry();
         final Context self = this;
@@ -67,16 +81,37 @@ public class TagTouchActivity extends Activity {
                 e.printStackTrace();
             }
         }
-*/
-        finish();
+         */
     }
 
-    private void startSettingActivityWithTagId(final String tagId) {
-        Intent newTagIntent = new Intent();
-        newTagIntent.putExtra(SettingActivity.TAGID_EXTRA, tagId);
-        newTagIntent.setClass(this, SettingActivity.class);
-        startActivity(newTagIntent);
-        finish();
+    private void setupTaskList() {
+        Task[] tasks = Task.getAll(this);
+        ArrayList<Task> taskList = new ArrayList<Task>();
+        for (Task task : tasks)
+            taskList.add(task);
+
+        ListView taskListView = (ListView)findViewById(R.id.tagTouchTaskList);
+        ArrayAdapter<Task> taskAdapter = new ArrayAdapter<Task>(this, R.layout.task_list_item, R.id.task_list_item_label, taskList);
+        taskListView.setAdapter(taskAdapter);
+
+        final Context self = this;
+        taskListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                LinearLayout layout = (LinearLayout)view;
+                TextView textView = (TextView)layout.findViewById(R.id.task_list_item_label);
+                Toast toast = Toast.makeText(self, "clicked " + textView.getText() + ", id:" + id + ", position:" + position, Toast.LENGTH_SHORT);
+                toast.show();
+
+                Tag tag = null;
+                if (Tag.isBrandNew(tagId, self)) {
+                    //                    tag = new Tag(tagId, null, null);
+                    tag = new Tag(tagId, "a", "b");
+                } else {
+                    tag = Tag.get(tagId, self);
+                }
+                tag.assignTask((Task)parent.getItemAtPosition(position), self);
+            }
+        });
     }
 
     private String getTagId(Intent intent) {
